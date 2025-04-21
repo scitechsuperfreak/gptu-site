2b2530fd7ab69224039c7212e772f4329048fb49
scitechsuperfreak <gabriel.driven@gmail.com>
2025-04-21 01:17:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const express = require('express');
-const authRoutes = require('./server/auth');
+import express from 'express';
+import authRoutes from './server/auth.js';
 
 const app = express();
 app.use(express.json());
 
-// Mount the router from auth.js
 app.use('/', authRoutes);
 
 const PORT = process.env.PORT || 3000;
