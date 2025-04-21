6157c366bafae2ef61dcbe5a1d0c25dc41c03641
scitechsuperfreak <gabriel.driven@gmail.com>
2025-04-21 02:18:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ import express from 'express';
 import authRoutes from './server/auth.js';
 
 const app = express();
-app.use(express.json()); // Enables parsing of JSON bodies
+app.use(express.json()); // Middleware to parse JSON
 
 app.use((req, res, next) => {
   console.log(`Received ${req.method} request to ${req.url}`);
   next();
 });
 
-app.use('/', authRoutes); // Mount the auth router
+app.use('/', authRoutes); // Mount auth routes
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
